2b8ab71696b8cb220e4ea75dc45217881bd2dc55
subo-9439 <kws3363@gmail.com>
2022-01-08 23:43:50 +0900

    h2데이터연결하기 + 소스 모듈화시켜서 학습

---
+++ sp-fastcampus-spring-sec/comp/user-admin/src/main/java/com/sp/fc/user/service/SpUserService.java
@@ -0,0 +1,56 @@
+package com.sp.fc.user.service;
+
+import com.sp.fc.user.domain.SpAuthority;
+import com.sp.fc.user.domain.SpUser;
+import com.sp.fc.user.repository.SpUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class SpUserService implements UserDetailsService {
+
+    private final SpUserRepository userRepository;
+
+    public SpUserService(SpUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findSpUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    public Optional<SpUser> findUser(String email) {
+        return userRepository.findSpUserByEmail(email);
+    }
+
+    public SpUser save(SpUser user) {
+        return userRepository.save(user);
+    }
+
+    public void addAuthority(Long userId, String authority) {
+        userRepository.findById(userId).ifPresent(user -> {
+            SpAuthority newRole = new SpAuthority(user.getUserId(), authority);
+            if(user.getAuthorities() == null){
+                HashSet<SpAuthority> authorities = new HashSet<>();
+                authorities.add(newRole);
+                user.setAuthorities(authorities);
+                save(user);
+            }else if(!user.getAuthorities().contains(newRole)){
+                HashSet<SpAuthority> authorities = new HashSet<>();
+                authorities.addAll(user.getAuthorities());
+                authorities.add(newRole);
+                user.setAuthorities(authorities);
+                save(user);
+            }
+        });
+    }
+}
---
+++ sp-fastcampus-spring-sec/server/login-userdetails/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.sp.fc.web.config;
+
+import com.sp.fc.user.service.SpUserService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@EnalbeGlobalMethodSecurity(prePostEnable = true)
+//역할에 설정되어 있는 페이지만 볼 수 있게해준다.
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final SpUserService userService;
+
+    public SecurityConfig(SpUserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+    //관리자는 유저의 권한을 사용할 수 있게 해줌
+    @Bean
+    RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(request->{
+                    request
+                            .antMatchers("/").permitAll()
+                            .anyRequest().authenticated()
+                            ;
+                })
+                .formLogin(
+                        login -> login.loginPage("/login")
+                                .permitAll()
+                                .defaultSuccessUrl("/", false)
+                                .failureUrl("/login-error")
+
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"))
+                ;
+    }
+
+    //css에 대해서 문제가 생겨버림 이럴 땐 web resource에 대해선 security필터가 적용되지 않도록 ignore를 해줘여한다.
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations(),
+                        PathRequest.toH2Console()
+                );
+    }
+}
