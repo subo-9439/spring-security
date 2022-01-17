2e9c667bce54a10ec218c23fde699b9faf537cbb
subo-9439 <kws3363@gmail.com>
2022-01-17 15:14:49 +0900

    권한처리

---
+++ sp-fastcampus-spring-sec/server/authority-test/src/test/java/com/sp/fc/web/test/AuthorityBasicTest.java
@@ -0,0 +1,24 @@
+package com.sp.fc.web.test;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AuthorityBasicTest extends WebIntegrationTest {
+
+    TestRestTemplate client;
+
+    @DisplayName("1. greeting 메세지를 불러온다.")
+    @Test
+    void test_1() {
+        client = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> response = client.getForEntity(uri("/greeting/subo"), String.class);
+
+        assertEquals("hello subo", response.getBody());
+        System.out.println(response.getBody());
+    }
+}
